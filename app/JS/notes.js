5c878059c1e5367b93ece5e682045753af48259a
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2024-11-18 12:29:08 -0500

    jtrhge

--- app/JS/notes.js
+++ app/JS/notes.js
@@ -2,7 +2,9 @@ async function getData() {
   //const response = await fetch("link of api");
   //console.log(response); // hey go out there and get this data from the outside, status 200 = got this data
   try {
-    const response = await fetch("https://whiskyhunter.net/api/");
+    const response = await fetch(
+      "https://date.nager.at/api/v3/publicholidays/2024/US"
+    );
     if (response.status != 200) {
       throw new Error(response); // do a error response, goes to the catch error
     } else {
