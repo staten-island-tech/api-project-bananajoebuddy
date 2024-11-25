9e211b6d6dd6e95919979fb0e30518e8fbf930e9
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2024-11-25 12:28:30 -0500

    ggads

--- app/JS/notes.js
+++ app/JS/notes.js
@@ -1,3 +1,38 @@
+// import { getData } from "./list.js"
+
+const DOMSelectors = {
+  container: document.getElementById("container"),
+};
+function createCards(data) {
+  DOMSelectors.container.innerHTML = "";
+  data.forEach((holiday) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+              <h3>${holiday.name}</h3>
+              <p><strong>Date:</strong> ${holiday.date}</p>
+              <p><strong>Type:</strong> ${holiday.type}</p>
+            </div>`
+    )
+  );
+}
+
+/* async function getDataAndCreateCards() {
+  try {
+    // Fetch the holiday data from the API using the imported function
+    const data = await getData();
+   
+    // Call function to render the cards with the fetched data
+    createCards(data);
+ 
+ 
+  } catch (error) {
+    // Handle errors (e.g., display a message)
+    console.error(error);
+    alert("sorry, couldn't find");
+  }
+ } */
+
 async function getData() {
   //const response = await fetch("link of api");
   //console.log(response); // hey go out there and get this data from the outside, status 200 = got this data
@@ -10,6 +45,7 @@ async function getData() {
     } else {
       const data = await response.json();
       console.log(data);
+      createCards(data);
     }
   } catch (error) {
     console.log(error);
@@ -20,6 +56,7 @@ async function getData() {
 
   // fetch returns a promise
 }
+
 getData();
 
 //core no
